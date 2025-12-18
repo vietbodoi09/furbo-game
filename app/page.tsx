528de3d9223d92744512a1d1f48137a325c7e4a9
vietbodoi09 <hviet09072007@gmail.com>
2025-12-18 09:39:46 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -259,6 +259,7 @@ export default function GamePage() {
             <PerformanceDashboard 
               sessionState={isEstablished(sessionState) ? sessionState : undefined}
               performanceStats={performanceStats}
+              chainData={chainData}
             />
           </div>
         </div>

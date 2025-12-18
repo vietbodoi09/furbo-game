d63127e502765d845d6aaf75758a31fbde08f75f
vietbodoi09 <hviet09072007@gmail.com>
2025-12-19 00:08:57 +0700

    Update FurboGameEngine.ts

--- lib/FurboGameEngine.ts
+++ lib/FurboGameEngine.ts
@@ -540,6 +540,12 @@ export class FurboGameEngine {
     };
     this.enemies.push(enemy);
   }
+  private checkCollision(rect1: any, rect2: any): boolean {
+      return rect1.x < rect2.x + rect2.width &&
+             rect1.x + rect1.width > rect2.x &&
+             rect1.y < rect2.y + rect2.height &&
+             rect1.y + rect1.height > rect2.y;
+  }
 
   // ========== REALTIME ACTION FUNCTIONS ==========
 
